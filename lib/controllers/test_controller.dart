277a843a7d8142933b3f74a17556818a5477f9da
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2025-07-03 23:24:13 +0800

    test

---
+++ lib/controllers/test_controller.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+
+class TestController  extends GetxController{
+  static  TestController get to => Get.find();
+
+  
+
+}
--- lib/core/getx/app_binding.dart
+++ lib/core/getx/app_binding.dart
@@ -2,11 +2,13 @@
 import 'package:get/get.dart';
 import 'package:kiosk/controllers/kiosk_controller.dart';
 import 'package:kiosk/controllers/sunmi_printer_controller.dart';
+import 'package:kiosk/controllers/test_controller.dart';
   
 class AppBinding extends Bindings {
   @override
   void dependencies() {
     Get.put(KioskController(), permanent: true);
     Get.put(SunmiPrinterController(), permanent: true);
+    Get.put(TestController(), permanent: true);
   }
 }
